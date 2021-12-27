9efeb00e23b51a8b24a0e123f7968c889b2bddaa
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2021-12-27 20:47:37 +0300

    program - четные числа

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int [] element (int length)
+{
+    int [] mas = new int [length];
+    return mas;
+}
+
+int [] sozdanie (int [] massiv, int min, int max)
+{
+    int index = 0;
+    while (index < massiv.Length)
+    {
+        massiv [index] = new Random().Next(min, max); 
+        index++;
+    }
+    return massiv;
+}
+
+string print (int [] arr)
+{
+    string res = String.Empty;
+    int index = 0;
+    while (index < arr.Length)
+    {
+        int array = arr[index];
+        Console.Write(array + " ");
+        index++;
+    }
+    return res;
+}
+
+
+int [] A = element(8);    
+sozdanie(A, -10, 10);
+print (A);
